d428a2162d77fae63556f5256550df17e1a60573
Bryan Santos <49541500+bryandevelops@users.noreply.github.com>
2023-09-28 09:19:09 -0400

    Create Event model

--- models/userModel.js
+++ models/userModel.js
@@ -4,11 +4,10 @@ import bcrypt from 'bcrypt';
 const userSchema = new mongoose.Schema({
   name: { type: String, required: [true, 'Please include a name'] },
   email: { type: String, required: [true, 'Please include an email'], lowercase: true, unique: true },
-  password: { type: String, required: [true, 'Please include a password'], minLength: 3 }
+  password: { type: String, required: [true, 'Please include a password'], minLength: 6 }
 }, { 
   timestamps: true,
-
-  // Even though it's hashed - don't serialize the password (won't return the password to us when finding)
+  // Even though it's hashed don't serialize the password (in other words, this won't return the password field to us when finding)
   toJSON: {
     transform: function(doc, ret) {
       delete ret.password;
@@ -19,9 +18,9 @@ const userSchema = new mongoose.Schema({
 
 // Mongoose pre-save hook (Mongoose middleware) that will hash the password anytime the password has changed
 userSchema.pre('save', async function(next) {
-  // 'this' is the user doc
+  // 'this' is the User document
 
-  // If the doc is unmodified, move on
+  // If the document is unmodified, move on
   if (!this.isModified('password')) return next();
 
   // Update the password with the computed hash
